27c7426510c4a11556bc6db2a14d28da53c2e0a9
retriquaz <retriquaz@gmail.com>
2019-01-07 21:50:29 +0300

    Add files via upload

--- commands/kurallar.js
+++ commands/kurallar.js
@@ -1,5 +1,5 @@
 exports.run = (bot, message, args) => {
-  if(message.content !== "tb kurallar"){
+  if(!message.content.startsWith("tb kurallar ")){
     return;
   } else{
        message.channel.send("" + message.mentions.members.first() + " Kaşgar Khanate Günde 3 kez kuralları okumanızı tavsiye eder.  " + message.guild.channels.find(channel => channel.name === "kurallar").toString() + "")
