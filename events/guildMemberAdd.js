f1523227d89845a85d691fe00780e81aa9e51444
retriquaz <retriquaz@gmail.com>
2019-06-07 16:12:14 +0300

    Add files via upload

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,7 +3,7 @@ module.exports = (bot, member) => {
  const genel = member.guild.channels.find(channel => channel.name === "geçici-konuşma-odası");
  const sıra = member.guild.channels.find(channel => channel.name === "yönetici-özel");
  if(genel){
- genel.send(`<@${member.user.id}> Hoşgeldin şu an onay sürecindesiniz. Onay alana kadar gerekli bilgileri okumanız tavsiye edilir.`+ member.guild.channels.find(channel => channel.name === "hoş-geldiniz").toString() + ` Bu kanaldan diğer onay bekleyenler ile sohbet edebilirsiniz. `);
+ genel.send(`<@${member.user.id}> Hoşgeldin şu an onay sürecindesiniz. Onay alana kadar gerekli bilgileri okumanız tavsiye edilir.`+ member.guild.channels.find(channel => channel.name === "geçici-konuşma-odası").toString() + ` Bu kanaldan diğer onay bekleyenler ile sohbet edebilirsiniz. Kuralları okumak için ` + member.guild.channels.find(channel => channel.name === "kurallar").toString() + ` kanalına gidiniz. `);
  }
  if(sıra){
    sıra.send(`<@${member.user.id}> Adlı kullanıcı onay beklemektedir.`);
