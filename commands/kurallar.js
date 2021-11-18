2121ca4d3afcdb900b9520e8d11954e62f121500
Göktuğ ÇOM <retriquaz@gmail.com>
2021-11-18 21:45:14 +0300

    Delete commands directory

--- commands/kurallar.js
+++
@@ -1,8 +0,0 @@
-exports.run = (bot, message, args) => {
-  if(!message.content.startsWith("tb kurallar ")){
-    return;
-  } else{
-       message.channel.send("" + message.mentions.members.first() + " Kaşgar Khanate Günde 3 kez kuralları okumanızı tavsiye eder.  " + message.guild.channels.find(channel => channel.name === "kurallar").toString() + "")
-       .catch(() => console.error('Kurallar kanalı bulunamadı.'));
-}
-}
