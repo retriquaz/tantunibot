fe92c5beeb5a0b36dcea07169614f69a258a5abb
Göktuğ ÇOM <retriquaz@gmail.com>
2021-11-18 21:38:16 +0300

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+exports.run = (bot, message, args) => {
+  if(message.content !== "tb help"){
+    return;
+  } else{
+
+  const embed = new Discord.RichEmbed()
+    .setColor(0xFF0000)
+    .setThumbnail(url='https://cdn.discordapp.com/attachments/453476121078661120/500399543267885066/logo.png')
+    .setAuthor("TantuniBot", "https://cdn.discordapp.com/avatars/484411378556338179/62f4ef653ecb1d495eca978ff1ebd781.png?size=128")
+    .setFooter("İçin Oluşturuldu", message.author.avatarURL)
+    .setTimestamp()
+    .setDescription(bot.helplist)
+
+    message.channel.send("" + message.author.toString() + " TantuniBot Emrinde", embed);
+}
+}
